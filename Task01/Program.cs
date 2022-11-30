47622fab02d9ff643d249d56b18e17bc2c42e81e
Maxsot <sotnikov.maxim3007@gmail.com>
2022-11-30 07:36:44 -0800

    Lessons task 01

---
+++ Task01/Program.cs
@@ -0,0 +1,38 @@
+﻿// 1. Напишите программу, 
+// которая принимает на вход трёхзначное число 
+// и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+int Prompt(string message)
+{
+    Console.Write($"{message} ");
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+
+
+
+
+}
+
+bool VAlidateNumber(int number)
+{
+    if (number >= 100 && number < 1000)
+    {
+        return true;
+
+    }
+    Console.WriteLine("Not Three Numbers");
+    return false;
+
+}
+int number = Prompt("Enter a number");
+if(VAlidateNumber(number))
+{
+    int result = number % 10;
+    Console.WriteLine($"Last number is {result}");
+}
+
+
+
