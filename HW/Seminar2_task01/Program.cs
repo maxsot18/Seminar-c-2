da84d69318e8fc9ac62fdf4de4e85d3632bb1c9a
maxsot18 <118660295+maxsot18@users.noreply.github.com>
2022-12-03 01:08:16 -0800

    Add files via upload

--- HW/Seminar2_task01/Program.cs
+++ HW/Seminar2_task01/Program.cs
@@ -1,10 +1,11 @@
-﻿// // Задача 1: Напишите программу, 
-// которая принимает на вход трёхзначное число и 
-// на выходе показывает вторую цифру этого числа. 
-// Не использовать строки
+﻿// Задача 2: Напишите программу, 
+// которая выводит случайное трёхзначное число и 
+// удаляет вторую цифру этого числа. 
+// Не использовать строки для расчета
 
-// // 456 -> 5
-// // 782 -> 8
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
 
 int Prompt(string message)
 {
@@ -14,13 +15,13 @@ int Prompt(string message)
 }
 int number = Prompt("Number = ");
 
-
 if (number >= 100 & number < 1000)
 {
-    double number1 = number % 100;
-    int number2 = Convert.ToInt32(number1);
-    int number3 = number2 / 10;
-    Console.WriteLine($"{number3}");
+    int number1 = number / 100;
+    double number2 = number % 10;
+    int number3 = Convert.ToInt32(number2);
+    Console.Write($"{number1}");
+    Console.Write($"{number3}");
+
 }
 else { Console.WriteLine("Not ThreeDigitNumber"); }
-
