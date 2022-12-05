1f104843f6e72c87a690f4c4038ada7ab2ec3566
Maxsot <sotnikov.maxim3007@gmail.com>
2022-12-05 05:38:21 -0800

    Task01

---
+++ TASK01/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 1: Напишите программу, 
+// которая принимает на вход число (А) 
+// и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int Prompt(string message)
+{
+    Console.WriteLine($"{message}");
+    return Convert.ToInt32(Console.ReadLine());
+}    
+
+int sumNumbers(int number)
+{
+    int sum = 0;
+    int i = 1;
+    while(i <= number)
+    {
+        sum +=i;
+        i++;
+    }
+    return sum;
+}
+
+int number = Prompt("Enter a number ");
+int result = sumNumbers(number);
+Console.WriteLine(result);
